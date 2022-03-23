70b2c40d7352fad09006bdb165e418c183bb1b66
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-03-23 13:50:16 +0000

    Fixed betty indentation

--- binary_trees.h
+++ binary_trees.h
@@ -6,19 +6,19 @@
 #include <stddef.h>
 
 /**
-* struct binary_tree_s - Binary tree node
-*
-* @n: Integer stored in the node
-* @parent: Pointer to the parent node
-* @left: Pointer to the left child node
-* @right: Pointer to the right child node
-*/
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+  int n;
+  struct binary_tree_s *parent;
+  struct binary_tree_s *left;
+  struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -44,4 +44,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 void binary_tree_print(const binary_tree_t *);
 
-#endif //BINARY_TREES_H
+#endif /*BINARY_TREES_H*/
